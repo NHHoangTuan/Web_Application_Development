d7a40c8ad8e2c24d21c99386096929e676dec407
NHHoangTuan <hoangtuan.study@gmail.com>
2024-11-06 19:40:48 +0700

    Switch to bcryptjs for better compatibility

--- web/GA04/Source/hagako-web/controllers/userController.js
+++ web/GA04/Source/hagako-web/controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 exports.getRegister = (req, res) => {
   res.render("users/register");
